517f4c3f36ab2d9c6d6c06cd86c440100bd2e3cd
Dineshgawas64 <dineshgawas64@gmail.com>
2023-02-02 11:43:27 +0530

    added schema file

---
+++ DEV/DB/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "menu"(
+    "menu_item_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "menu_item_name" TEXT NOT NULL,
+    "menu_item_description" TEXT NOT NULL,
+    "menu_item_price" INTEGER NOT NULL
+);
+
+CREATE TABLE "cart"(
+    "cart_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "menu_item_id" TEXT NOT NULL,
+    "order_quantity" TEXT NOT NULL,
+    "customer_name" TEXT NOT NULL,
+    FOREIGN KEY ("menu_item_id") REFERENCES "menu"("menu_item_id")
+);
