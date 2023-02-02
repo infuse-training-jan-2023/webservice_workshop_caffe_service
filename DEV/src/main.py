37a1874d0428d63fb4d5a17dea6d6f8dac6d6c2f
Dineshgawas64 <dineshgawas64@gmail.com>
2023-02-02 12:06:13 +0530

    added main.py  and demo routes

---
+++ DEV/src/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response 
+# import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to CAFFE SERVICE'
+
+@app.route('/menu/all')
+def display_all_menu_items():
+    return 'menu 1'
+
+@app.route('/cart/<string:cust_name>')
+def display_all_cart_items(cust_name):
+    return cust_name
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port = 5001,host = "0.0.0.0")
